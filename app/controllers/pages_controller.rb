d0779163bb1ae31a79a3923723a3ddd3ae0f8c7e
John Knapp <jk@johnknapp.com>
2017-02-12 20:51:07 -0800

    home page is a faq

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
-    respond_to do |format|
-      format.html { render layout: 'wide_layout'}
-    end
+    # respond_to do |format|
+    #   format.html { render layout: 'wide_layout'}
+    # end
   end
 
   def join_thanks
