a29ac45341807d1f75eb2b31d598428f57c9b96b
John Knapp <jk@johnknapp.com>
2017-02-17 15:11:21 -0800

    owner edit company. confirmed user create company.

--- app/models/company_user.rb
+++ app/models/company_user.rb
@@ -2,7 +2,7 @@ class CompanyUser < ApplicationRecord
   belongs_to :company
   belongs_to :user
   enum equity: {
-      no_equity:  0,
+      equity?:    0,
       under_5:    1,
       over_5:     2,
       over_10:    3,
