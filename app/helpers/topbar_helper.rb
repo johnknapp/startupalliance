98520612d9ae4a03eaa19b3fb0997274e9054979
John Knapp <jk@johnknapp.com>
2017-02-18 21:48:57 -0800

    I should get more rest

---
+++ app/helpers/topbar_helper.rb
@@ -0,0 +1,25 @@
+module TopbarHelper
+  def menu_icon
+    [
+      "<i class='fa fa-thumbs-o-up'></i>",
+      "<i class='fa fa-thumbs-o-up'></i>",
+      "<i class='fa fa-thumbs-o-up'></i>",
+      "<i class='fa fa-thumbs-o-up'></i>",
+      "<i class='fa fa-thumbs-o-up'></i>",
+      "<i class='fa fa-thumbs-o-up'></i>",
+      "<i class='fa fa-hand-o-right'></i>",
+      "<i class='fa fa-hand-o-right'></i>",
+      "<i class='fa fa-hand-o-right'></i>",
+      "<i class='fa fa-hand-o-right'></i>",
+      "<i class='fa fa-hand-o-right'></i>",
+      "<i class='fa fa-hand-o-right'></i>",
+      "<i class='fa fa-hand-peace-o'></i>",
+      "<i class='fa fa-hand-spock-o'></i>",
+      "<i class='fa fa-hand-rock-o'></i>",
+      "<i class='fa fa-hand-paper-o'></i>",
+      "<i class='fa fa-hand-scissors-o'></i>"
+    ].sample.html_safe
+  end
+end
+
+
