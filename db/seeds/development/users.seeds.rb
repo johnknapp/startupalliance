6647219f5b81080c29964ba675e7082e4fc0bce4
John Knapp <jk@johnknapp.com>
2017-07-31 14:48:17 -0700

    copy tweaks

--- db/seeds/development/users.seeds.rb
+++ db/seeds/development/users.seeds.rb
@@ -1,16 +1,37 @@
 User.destroy_all
 
-admin = User.new(first_name: 'Ima', last_name: 'Admin', email: 'admin@example.com', username: 'ima-admin',
-                 time_zone: 'Pacific Time (US & Canada)', role: 'john', state: 'active',
+admin = User.new(first_name: 'Ima', last_name: 'Admin', email: 'admin@example.com', username: 'admin',
+                 time_zone: 'Pacific Time (US & Canada)', role: 'admin', state: 'active',
                  password: 'password', password_confirmation: 'password'
               )
 admin.skip_confirmation!
 admin.save
 
-user = User.new(first_name: 'Ima', last_name: 'User', email: 'user@example.com', username: 'ima-user',
-                time_zone: 'Eastern Time (US & Canada)', role: 'user', state: 'active',
+guest = User.new(first_name: 'Guest', last_name: 'User', email: 'guest@example.com', username: 'guest',
+                time_zone: 'Eastern Time (US & Canada)', role: 'user', state: 'active', plan: 'guest',
                 password: 'password', password_confirmation: 'password'
               )
-user.skip_confirmation!
-user.save
+guest.skip_confirmation!
+guest.save
+
+associate = User.new(first_name: 'Associate', last_name: 'User', email: 'user@example.com', username: 'associate',
+                time_zone: 'Eastern Time (US & Canada)', role: 'user', state: 'active', plan: 'associate',
+                password: 'password', password_confirmation: 'password'
+              )
+associate.skip_confirmation!
+associate.save
+
+alliance = User.new(first_name: 'Alliance', last_name: 'User', email: 'alliance@example.com', username: 'alliance',
+                time_zone: 'Eastern Time (US & Canada)', role: 'user', state: 'active', plan: 'alliance',
+                password: 'password', password_confirmation: 'password'
+              )
+alliance.skip_confirmation!
+alliance.save
+
+company = User.new(first_name: 'Company', last_name: 'User', email: 'company@example.com', username: 'company',
+                time_zone: 'Eastern Time (US & Canada)', role: 'user', state: 'active', plan: 'company',
+                password: 'password', password_confirmation: 'password'
+              )
+company.skip_confirmation!
+company.save
 
