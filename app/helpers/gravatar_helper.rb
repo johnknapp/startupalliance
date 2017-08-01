928927493686880722938e19069300049d5b90f5
John Knapp <jk@johnknapp.com>
2017-07-31 18:04:24 -0700

    must. have. fun.

--- app/helpers/gravatar_helper.rb
+++ app/helpers/gravatar_helper.rb
@@ -1,6 +1,9 @@
+
+# default options: https://en.gravatar.com/site/implement/images/
+
 module GravatarHelper
   def gravatar_url(email, size)
     gravatar = Digest::MD5::hexdigest(email).downcase
-    url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
+    url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}&d=wavatar"
   end
 end
