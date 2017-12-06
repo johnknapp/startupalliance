fca94dda48a34bcc863c35113361075fe095e20c
John Knapp <jk@johnknapp.com>
2017-12-06 15:40:40 -0800

    user profile onboarding UA text. helper cleanup.

--- app/helpers/alliance_helper.rb
+++ app/helpers/alliance_helper.rb
@@ -2,8 +2,7 @@ module AllianceHelper
 
   # return alliance member scoped company_user records
   def alliance_company_team(alliance,company)
-    alliance_user_ids = alliance.members.pluck(:user_id)
-    CompanyUser.where(company_id: company.id).where('user_id in (?)', alliance_user_ids)
+    CompanyUser.where(company_id: company.id).where('user_id in (?)', alliance.members.pluck(:user_id))
   end
 
 end
