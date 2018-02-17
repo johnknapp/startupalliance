88c83cec3d894c91cebd3845664fade80f95f10f
John Knapp <jk@johnknapp.com>
2018-02-16 17:38:01 -0800

    discussion refactoring wrapup

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   def nested_posts(posts) # posts is a nested hash from the ancestry arrange method
     posts.map do |post, sub_posts|
-      render('partials/post', post: post) + content_tag(:div, nested_posts(sub_posts), class: 'nested_posts')
+      render('post/show_modal', post: post) + content_tag(:div, nested_posts(sub_posts), class: 'nested_posts')
     end.join.html_safe
   end
 end
