6e29ded1972c506fe160e64b2ae9f89514c45bda
John Knapp <jk@johnknapp.com>
2018-03-04 09:40:33 -0800

    require account activation to post, reply or author/edit kb pages.

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -2,7 +2,7 @@ module ForumsHelper
 
   def nested_posts(posts) # posts is a nested hash from the ancestry arrange method
     posts.map do |post, sub_posts|
-      render('post/show_modal', post: post) + content_tag(:div, nested_posts(sub_posts), class: 'nested_posts')
+      render('post/show', post: post) + content_tag(:div, nested_posts(sub_posts), class: 'nested_posts')
     end.join.html_safe
   end
 
