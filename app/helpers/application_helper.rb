aa5313185a8dcdb3f396f9ae872d1d91c8e51e4b
John Knapp <jk@johnknapp.com>
2018-03-07 12:53:10 -0800

    markdown for posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,26 @@ module ApplicationHelper
     url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}&d=wavatar"
   end
 
+  def markdown(text)
+    options = {
+        filter_html:     true,
+        # no_images:       true,
+        hard_wrap:       true,
+        link_attributes: { rel: 'nofollow', target: "_blank" },
+        space_after_headers: true,
+        fenced_code_blocks: true
+    }
+
+    extensions = {
+        autolink:           true,
+        superscript:        true,
+        disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
+
 end
