8a8fb379e4bfe06cbec934d7c252e0d72845789e
John Knapp <jk@johnknapp.com>
2018-03-08 11:19:21 -0800

    add default work role

--- db/migrate/20180308182436_add_work_role_to_users.rb
+++ db/migrate/20180308182436_add_work_role_to_users.rb
@@ -1,5 +1,5 @@
 class AddWorkRoleToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :work_role, :string
+    add_column :users, :work_role, :string, default: 'Unknown'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -365,7 +365,7 @@ ActiveRecord::Schema.define(version: 20180308182436) do
     t.integer  "pages_count",             default: 0,             null: false
     t.integer  "posts_count",             default: 0,             null: false
     t.integer  "topics_count",            default: 0,             null: false
-    t.string   "work_role"
+    t.string   "work_role",               default: "Unknown"
     t.index "lower((username)::text) text_pattern_ops", name: "users_username_lower", unique: true, using: :btree
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
