ace27166085bb5da4ed7914cb3791f12a4cd2693
John Knapp <jk@johnknapp.com>
2018-09-30 13:36:01 -0700

    timezones!

--- app/controllers/concerns/date_converter.rb
+++ app/controllers/concerns/date_converter.rb
@@ -4,14 +4,14 @@ module DateConverter
 
     def string_to_date(string)
       if string.present?
-        Time.strptime(string, '%m/%d/%Y').to_date
+        Time.zone.strptime(string, '%m/%d/%Y').to_date
       end
     end
 
     # 'mm/dd/yyyy hh:mm' to Fri, 01 Jan 2016 00:00:00 -0800
     def string_24h_to_datetime(string)
       if string.present?
-        Time.strptime(string, '%m/%d/%Y %H:%M').to_datetime
+        Time.zone.strptime(string, '%m/%d/%Y %H:%M').to_datetime
       else
         nil
       end
