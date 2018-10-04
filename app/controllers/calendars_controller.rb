44c767a7eb67001d73dee0b4613151fbe33b21f2
John Knapp <jk@johnknapp.com>
2018-10-04 12:59:30 -0700

    time zone support, woot!

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,15 +1,19 @@
 class CalendarsController < ApplicationController
+  require 'icalendar/tzinfo'
 
   def upcoming_events
     events = Event.upcoming
     respond_to do |format|
       format.ics do
         cal = Icalendar::Calendar.new
+        cal.timezone do |t|
+          t.tzid              = 'UTC'
+        end
         cal.x_wr_calname = 'Startup Alliance Events'
         events.each do |upcoming_event|
           event               = Icalendar::Event.new
-          event.dtstart       = upcoming_event.start_time.utc
-          event.dtend         = upcoming_event.start_time.utc+1.hour
+          event.dtstart       = upcoming_event.start_time
+          event.dtend         = upcoming_event.start_time+1.hour
           event.summary       = '[SA] ' + upcoming_event.title
           event.url           = event_url(upcoming_event)
           cal.add_event(event)
